cc275cb2b672fb372adce33ddcbc7aa5c3639e78
Felix Singer <singer.felix@t-online.de>
2018-01-21 09:25:52 +0100

    scripts: Add script to configure the screen layout

---
+++ .scripts/screenlayout.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output DVI-D-1-1 --off --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI-1-4 --off --output DVI-I-1-1 --off --output HDMI3 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --off --output DP-1-3 --off
